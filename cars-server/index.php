b24e166e2a0f97e61db65e62d2952603fdbf30e3
mhdzz1 <110099308+mhdzz1@users.noreply.github.com>
2025-11-12 11:52:18 +0200

    Your commit message for submodule changes

--- cars-server/index.php
+++ cars-server/index.php
@@ -1,10 +1,10 @@
 <?php 
 require_once("./services/ResponseService.php");
 
-$base_dir = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
-$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$base_dir = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');//http://localhost/phpmyadmin/
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);/CARS/AHMAD/ZEHRA
 
-if (strpos($request, $base_dir) === 0) {
+if (strpos($request /*http://localhost/phpmyadmin/*/, $base_dir/*http://localhost/phpmyadmin/*/) === 0) {
     $request = substr($request, strlen($base_dir));
 }
 
@@ -15,18 +15,18 @@ if ($request == '') {
 //array of routes - a mapping between routes and controller name and method!
 //remove routes from here!! 
 $apis = [
-    '/cars'         => ['controller' => 'CarController', 'method' => 'getCarByID'],
+    '/cars'         => ['controller' => 'CarController', 'method' => 'getCarByID'],//http://localhost/phpmyadmin/CARS
     '/users'         => ['controller' => 'UserController', 'method' => 'getUsers']
 ];
 
-if (isset($apis[$request])) {
+if (isset($apis[$request])) {//CARS
     $controller_name = $apis[$request]['controller']; 
     $method = $apis[$request]['method'];
     require_once "controllers/{$controller_name}.php";
     
     $controller = new $controller_name();
     if (method_exists($controller, $method)) {
-        $controller->$method();
+        $controller->$method();//NON SSTATIC ::
     } else {
         echo ResponseService::response(500, "Error: Method {$method} not found in {$controller_name}");
     }
--- cars-server/models/Model.php
+++ cars-server/models/Model.php
@@ -1,28 +1,62 @@
 <?php
 abstract class Model{
 
-    protected static string $table;
-    //protected static string $primary_key = "id";
+    protected static string $tzable;
+    protected static string $primary_key = "id";
+   
 
-    public static function find(mysqli $connection, string $id, string $primary_key = "id"){
-        $sql = sprintf("SELECT * from %s WHERE %s = ?",
-                       static::$table,
-                       $primary_key);
-                       //static::$primary_key);
+    // public static function find(mysqli $connection, string $id){//read
+    //     $sql = sprintf("SELECT * from %s WHERE %s = ?",
+    //                    static::$table,
+    //                     //    $primary_key);
+    //                    static::$primary_key);
 
-        $query = $connection->prepare($sql);
-        $query->bind_param("i", $id);
-        $query->execute();               
+    //     $query = $connection->prepare($sql);
+    //     $query->bind_param("i", $id);
+    //     $query->execute();               
 
-        $data = $query->get_result()->fetch_assoc();
+    //     $data = $query->get_result()->fetch_assoc();
 
-        return $data ? new static($data) : null;
+    //     return $data ? new static($data) : null;
+    // }
+
+    //     public static function findall(mysqli $connection){//crud
+    //     $sql = sprintf("SELECT * from %s",
+    //                    static::$table,
+    //                     //    $primary_key);
+    //                    static::$primary_key);
+
+    //     $query = $connection->prepare($sql);
+    //     $query->execute();               
+
+    //     $data = $query->get_result()->fetch_assoc();
+
+    //     return $data ? new static($data) : null;
+    // }
+    //     public static function create(mysqli $connection,int $id, string $name, string $color,int $year){//crud
+    //     $stmt = $connection->prepare("INSERT INTO cars (id,name,color,year) VALUES (?, ?,?, ?)");
+    //     $stmt->bind_param("issi",$id,$name,$color,$year); 
+    //     $stmt->execute()    
+    //      $stmt->close();          
+
+    // }
+
+    // public static function update(mysqli $connection,int $id, string $name, string $color,int $year){//crud
+    //     $stmt = $connection->prepare("UPDATE  cars SET name=?, color=?,year=? where id=?");
+    //     $stmt->bind_param("ssii",$name,$color,$year,$id); 
+    //     $stmt->execute()    
+    //      $stmt->close();          
+
+    // }
+  public static function delete(mysqli $connection,int $id){//crud
+        $stmt = $connection->prepare("DELETE FROM CARS WHERE id=?");
+        $stmt->bind_param("i",$id); 
+        $stmt->execute()    
+         $stmt->close();          
+
     }
 
-    public static function findAll(mysqli $connection){
-        //implement this
-    }
-
+    
 }
 
 
